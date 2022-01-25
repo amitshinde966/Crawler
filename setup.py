c4048d5289928426a2689cd025b48aa7df551eae
Pranav <pranavparge@gmail.com>
2022-01-25 20:00:26 +0530

    Library Changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='crawler',
+    name='exulercrawler',
     packages=find_packages(include=['src']),
     version='1.0.0',
     description='Exuler Crawler',
